01eda228f0871f76edcbcdbb9de2de3e82a800cd
ecarranzap <ecarranzap@gmail.com>
2017-04-19 21:39:55 -0500

    Es el inicio del proyecto

---
+++ segunda.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Segunda Página en PHP</title>
+    <link rel="stylesheet" href="/css/main.css">
+  </head>
+  <body>
+    <p>Esta es una prueba de color heredado del estilo</p>
+    <p class="principal">prueba de herencia de estilos</p>
+  </body>
+</html>
